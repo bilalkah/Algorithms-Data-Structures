814f43b2664cf06c461d97c87af629b145faee6e
Bilal Kahraman <45990633+bilalkah@users.noreply.github.com>
2021-01-27 12:46:37 +0300

    add prepend, insert and delete function

--- Linked-List/Single/main.c
+++ Linked-List/Single/main.c
@@ -9,6 +9,9 @@ typedef struct Node
 
 node *createNode(int);
 node *appendList(node *, int);
+node *prependList(node *, int);
+node *insertList(node *, int, int);
+node *deleteElement(node *, int);
 void printList(node *);
 void deleteList(node *);
 
@@ -17,6 +20,8 @@ int main()
 
     node *head = NULL;
 
+    head = prependList(head, 85);
+    printList(head);
     head = appendList(head, 5);
     head = appendList(head, 6);
     head = appendList(head, 10);
@@ -24,6 +29,19 @@ int main()
     head = appendList(head, 7);
     head = appendList(head, 95);
 
+    printList(head);
+
+    head = prependList(head, 21);
+
+    printList(head);
+
+    head = prependList(head, 22);
+    printList(head);
+
+    head = insertList(head, 31, 7);
+    printList(head);
+
+    head = deleteElement(head, 100);
     printList(head);
 
     deleteList(head);
@@ -71,6 +89,76 @@ node *appendList(node *head, int val)
     return head;
 }
 
+/**
+ * @brief Prepend val to list
+ * 
+ * @param head 
+ * @param val 
+ * @return node* 
+ */
+node *prependList(node *head, int val)
+{
+    node *p = createNode(val);
+    if (head == NULL)
+    {
+        return p;
+    }
+    p->next = head;
+    return p;
+}
+
+/**
+ * @brief Insert val to list after where
+ * 
+ * @param head 
+ * @param val 
+ * @param where 
+ * @return node* 
+ */
+node *insertList(node *head, int val, int where)
+{
+    node *p;
+    for (p = head; p != NULL && p->value != where; p = p->next)
+        ;
+    if (p == NULL)
+    {
+        printf("%d not exists.\n", where);
+        return head;
+    }
+    node *t = createNode(val);
+    t->next = p->next;
+    p->next = t;
+    return head;
+}
+
+/**
+ * @brief Delete selected node of list
+ * 
+ * @param head 
+ * @param val 
+ * @return node* 
+ */
+node *deleteElement(node *head, int val)
+{
+    node *p, *prev;
+    for (p = head; p != NULL && p->value != val; prev = p, p = p->next)
+        ;
+    if (p == NULL)
+    {
+        printf("%d not exists.\n", val);
+        return head;
+    }
+    if (p == head)
+    {
+        head = head->next;
+        free(p);
+        return head;
+    }
+    prev->next = p->next;
+    free(p);
+    return head;
+}
+
 void printList(node *head)
 {
     node *p;
@@ -82,6 +170,7 @@ void printList(node *head)
             printf("->");
         }
     }
+    printf("\n");
 }
 
 void deleteList(node *head)
