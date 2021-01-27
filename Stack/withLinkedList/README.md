3f0e7989adfb72cc9f4cceb369f15d227520f01a
Bilal Kahraman <45990633+bilalkah@users.noreply.github.com>
2021-01-27 22:17:46 +0300

    Stack/with LinkedList folder created

---
+++ Stack/withLinkedList/README.md
@@ -0,0 +1,7 @@
+# Stack with Linked List
+- bool isEmpty(stack *LIFO) : Check stack is empty or not. 
+- bool isFull(stack *LIFO) : Check stack is full or not.
+- int peek(stack *LIFO) : Peek last value of stack.
+- stack *push(stack *LIFO, int val) : Push new value to stack, if it is full, nothing will happen. 
+- stack *pop(stack *LIFO, int *val) : Pop value from stack, if it is empty, nothing will happen.  
+
---
+++ Stack/withLinkedList/main.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX 6
+
+typedef struct Stack
+{
+    int value;
+    struct Stack *next;
+} stack;
+
+static int count;
+
+bool isEmpty(stack *);
+bool isFull(stack *);
+int peek(stack *);
+stack *push(stack *, int);
+stack *pop(stack *, int *);
+
+int main()
+{
+
+    stack *LIFO = NULL;
+    int val;
+
+    LIFO = push(LIFO, 10);
+    LIFO = push(LIFO, 15);
+    LIFO = push(LIFO, 22);
+    LIFO = push(LIFO, 36);
+    LIFO = push(LIFO, 20);
+    LIFO = push(LIFO, 16);
+
+    printf("Peek from stack: %d\n",peek(LIFO));
+    LIFO = push(LIFO, 37);
+
+    while (!isEmpty(LIFO))
+    {
+        LIFO = pop(LIFO, &val);
+        printf("%d ", val);
+    }
+
+    return 0;
+}
+
+/**
+ * @brief Check stack is empty or not
+ * 
+ * @param LIFO 
+ * @return true 
+ * @return false 
+ */
+bool isEmpty(stack *LIFO)
+{
+    if (LIFO == NULL)
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Check stack is full or not
+ * 
+ * @param LIFO 
+ * @return true 
+ * @return false 
+ */
+bool isFull(stack *LIFO)
+{
+    if (count == MAX)
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief Peek last value 
+ * 
+ * @param LIFO 
+ * @return int 
+ */
+int peek(stack *LIFO)
+{
+    return LIFO->value;
+}
+
+/**
+ * @brief Push value to stack
+ * 
+ * @param LIFO 
+ * @param val 
+ * @return stack* 
+ */
+stack *push(stack *LIFO, int val)
+{
+    if (isFull(LIFO))
+    {
+        printf("Stack is Full!\n");
+        return LIFO;
+    }
+    stack *p = (stack *)malloc(sizeof(stack));
+    p->value = val;
+    p->next = LIFO;
+    count++;
+    return p;
+}
+
+/**
+ * @brief Pop value from stack
+ * 
+ * @param LIFO 
+ * @param val 
+ * @return stack* 
+ */
+stack *pop(stack *LIFO, int *val)
+{
+    if (isEmpty(LIFO))
+    {
+        printf("Stack is Empty!\n");
+        (*val) = -1;
+        return LIFO;
+    }
+    (*val) = LIFO->value;
+    stack *p = LIFO;
+    LIFO = LIFO->next;
+    free(p);
+    count--;
+    return LIFO;
+}
